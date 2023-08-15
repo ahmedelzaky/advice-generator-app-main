32024afab4b2491859d8fbe561140f7044134ad6
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-08-15 21:11:09 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const api = "https://api.adviceslip.com/advice";
+let id = document.querySelector("#qoute-id");
+let advice = document.querySelector(".advice-content span");
+let icon = document.querySelector(".icon-box");
+
+async function getData() {
+  let res = await fetch(api);
+  let data = await res.json();
+  updateUi(data.slip);
+}
+
+function updateUi(qoute) {
+  console.log(qoute);
+  id.textContent = qoute.id;
+  advice.textContent = qoute.advice;
+}
+
+getData();
+
+setInterval(() => {
+  getData();
+}, 5000);
+
+icon.addEventListener("click", () => {
+  window.location = "https://www.linkedin.com/in/ahmed-el-zaki/";
+});
